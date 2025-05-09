14260b60374a8a131dd6e23543d2d34b901bbdc1
Moussa <salisoumoussa1@gmail.com>
2025-05-09 11:28:09 +0200

    Aggiunto supporto per la ricerca di 'Selenium' nel campo di input

--- SeleniumFirstApplication/SimpleApplciationRunner.cs
+++ SeleniumFirstApplication/SimpleApplciationRunner.cs
@@ -1,5 +1,5 @@
 ﻿using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
 using WebDriverManager;
 using WebDriverManager.DriverConfigs.Impl;
 
@@ -9,11 +9,41 @@ namespace SeleniumFirstApplication
     {
         public static void Main(string[] args)
         {
-            new DriverManager().SetUpDriver(new ChromeConfig());
+            try
+            {
+                // Configura il driver
+                new DriverManager().SetUpDriver(new FirefoxConfig());
 
-            IWebDriver driver = new ChromeDriver();
+                FirefoxOptions options = new FirefoxOptions();
+                options.BrowserExecutableLocation = @"C:\Program Files\Mozilla Firefox\firefox.exe";
 
-            driver.Navigate().GoToUrl("https://www.github/sqlisou");
+                IWebDriver driver = new FirefoxDriver(options);
+                driver.Manage().Window.Maximize();
+
+                // Naviga alla pagina principale
+                driver.Navigate().GoToUrl("https://www.github.com");
+
+                // Trova e interagisci con il campo di ricerca
+                //WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
+                //IWebElement searchInput = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("query-builder-test")));
+
+                // Trova e interagisci con il campo di ricerca
+                IWebElement searchbox = driver.FindElement(By.CssSelector(".search-input"));
+                searchbox.Click();
+
+                IWebElement searchInput = driver.FindElement(By.CssSelector("#query-builder-test"));
+
+                // Inserisci "Selenium" nel campo di input
+                searchInput.SendKeys("Selenium");
+                searchInput.SendKeys(Keys.Enter);
+
+                // Chiudi il driver
+                //driver.Quit();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Errore durante il setup del driver: {ex.Message}");
+            }
         }
     }
 }
